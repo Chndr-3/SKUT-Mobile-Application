b40e62a385ea207666c6c6dc78ba0151d49f2eb3
Chandra <chandrawjy391@gmail.com>
2022-05-17 16:04:00 +0700

    Adding bottom navigation feature to the App

---
+++ app/src/main/java/com/bangkit/skutapp/HomeFragment.kt
@@ -0,0 +1,58 @@
+package com.bangkit.skutapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.bangkit.skutapp.databinding.ActivityMainBinding
+import com.bangkit.skutapp.databinding.FragmentHomeBinding
+import com.bangkit.skutapp.model.ViewPagerItem
+import java.util.ArrayList
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private val list = ArrayList<ViewPagerItem>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        list.addAll(listViewPagerItem)
+
+    }
+    private val listViewPagerItem: ArrayList<ViewPagerItem>
+        get() {
+            val dataQuestion = resources.getStringArray(R.array.data_question)
+            val dataAnswer = resources.getStringArray(R.array.data_answer)
+            val listTanyaJawab = ArrayList<ViewPagerItem>()
+            for (i in dataQuestion.indices) {
+                val hero = ViewPagerItem(dataQuestion[i],dataAnswer[i])
+                listTanyaJawab.add(hero)
+            }
+            return listTanyaJawab
+        }
+    private fun showViewPager() {
+        binding.viewPager.adapter = ViewPagerAdapter(list)
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.circleIndicator.setViewPager(binding.viewPager)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.getRoot();
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showViewPager()
+    }
+
+
+    companion object {
+
+    }
+}
--- app/src/main/java/com/bangkit/skutapp/MainActivity.kt
+++ app/src/main/java/com/bangkit/skutapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bangkit.skutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 import androidx.viewpager2.widget.ViewPager2
 import com.bangkit.skutapp.databinding.ActivityMainBinding
@@ -9,30 +10,40 @@ import com.bangkit.skutapp.model.ViewPagerItem
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val list = ArrayList<ViewPagerItem>()
+   private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        list.addAll(listViewPagerItem)
-        showRecyclerList()
-        binding.circleIndicator.setViewPager(binding.viewPager)
+        selection()
+
+
     }
-    private val listViewPagerItem: ArrayList<ViewPagerItem>
-        get() {
-            val dataQuestion = resources.getStringArray(R.array.data_question)
-            val dataAnswer = resources.getStringArray(R.array.data_answer)
-            val listTanyaJawab = ArrayList<ViewPagerItem>()
-            for (i in dataQuestion.indices) {
-                val hero = ViewPagerItem(dataQuestion[i],dataAnswer[i])
-                listTanyaJawab.add(hero)
+
+    private fun selection() {
+        loadFragment(HomeFragment())
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                }
+//                R.id.camera -> {
+//                    loadFragment(ChatFragment())
+//                    return@setOnNavigationItemReselectedListener
+//                }
+//                R.id.profile -> {
+//                    loadFragment(SettingFragment())
+//                    return@setOnNavigationItemReselectedListener
+//                }
             }
-            return listTanyaJawab
         }
-    private fun showRecyclerList() {
-        binding.viewPager.adapter = ViewPagerAdapter(list)
-        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
     }
+        private fun loadFragment(fragment: Fragment){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container,fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
 }
